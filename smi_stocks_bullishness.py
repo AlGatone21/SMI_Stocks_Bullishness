afa2fb6cb173a98f9d5ee7f7a8058fa4e0fbe0a5
AlGatone21 <163826091+AlGatone21@users.noreply.github.com>
2024-05-02 09:53:09 +0200

    Add files via upload

---
+++ smi_stocks_bullishness.py
@@ -0,0 +1,220 @@
+############################################################################################################
+# S&PStock Bullishness Sentiment Analysis
+###########################§################################################################################
+
+import streamlit as st
+import yfinance as yf
+from gnews import GNews 
+from tqdm import tqdm
+import pandas as pd
+from datetime import datetime
+from newspaper import Article
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import plotly.graph_objects as go
+
+
+
+
+############################################################################################################
+# Functions defnitons                                                                                      
+############################################################################################################
+@st.cache_data()
+def get_stock_current_price(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        data = stock.history(period="1d")
+        return round(data['Close'][0], 2)
+    except:
+        return "data not available"
+@st.cache_data()
+def get_stock_returns(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        data = stock.history(period="1mo")
+        return round(data['Close'][-1] - data['Open'][0], 2)
+    except:
+        return "data not available"
+@st.cache_data()
+def get_stock_series(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        data = stock.history(period="1mo")
+        return data['Close']
+    except:
+        return "data not available"
+
+@st.cache_data()
+def get_stock_news(com):
+    google_news = GNews(period="7d", max_results=10)
+    df = google_news.get_news(company)
+
+    url_data = []
+    for i in range(0,len(df)):
+        url_data.append(df[i]["url"])
+        
+    title_data = []
+    for i in range(0,len(df)):
+        title_data.append(df[i]["title"])
+        
+    published_date_data = []
+    for i in range(0,len(df)):
+        date_string = df[i]["published date"]
+        date_format = "%a, %d %b %Y %H:%M:%S %Z"
+        date_object = datetime.strptime(date_string, date_format)
+        new_date_format = "%d/%m/%Y"
+        new_date = date_object.strftime(new_date_format)
+        date_object = datetime.strptime(new_date, new_date_format)
+        published_date_data.append(date_object)
+
+    publisher_data = []
+    for i in range(0,len(df)):
+        publisher_data.append(df[i]["publisher"]["title"])
+
+    df = pd.DataFrame({"Title": title_data, "URL" : url_data, "Date" : published_date_data, "Publisher" : publisher_data})
+
+    global progress_bar
+    progress_bar = st.progress(0)
+    text_list = []
+    for i in tqdm(range(0, len(df))):
+        article = Article(df["URL"][i])
+        try:
+            article.download()
+            article.parse()
+            text = article.text
+            text_list.append(text)
+            progress_bar.progress(i/len(df))    
+        except:
+            text_list.append("not_found")
+            progress_bar.progress(i/len(df))
+
+    progress_bar.empty()
+    df["Text"] = text_list
+    news = df[df["Text"] != "not_found"]
+    return news
+
+sp500 = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")[0] # get the S&P 500 companies from wikipedia
+
+@st.cache_data()
+def analyze_news_sentiment(news):
+    # Load the pre-trained sentiment analysis model
+    model_name = "ProsusAI/finbert"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+    # Define a function to preprocess the text
+    def preprocess_text(text):
+        # Tokenize the text
+        inputs = tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            return_tensors="pt",
+            truncation=True,
+            max_length=512
+        )
+
+        # Return the preprocessed inputs
+        return inputs
+
+    # Define a function to predict the sentiment
+    def predict_sentiment(inputs):
+        # Make predictions
+        outputs = model(**inputs)
+        logits = outputs.logits
+        probabilities = torch.softmax(logits, dim=1).detach().numpy()[0]
+
+        # Get the predicted sentiment label
+        labels = ["negative", "neutral", "positive"]
+        predicted_label = labels[probabilities.argmax()]
+
+        # Return the predicted sentiment label and probabilities
+        return predicted_label, probabilities
+
+    # Analyze the sentiment of each news article
+    sentiment_data = []
+    counter = 0
+    for article in news["Text"]:
+        # Preprocess the text
+        inputs = preprocess_text(article)
+
+        # Predict the sentiment
+        sentiment_label, sentiment_probabilities = predict_sentiment(inputs)
+
+        # Append the sentiment label and probabilities to the sentiment data
+        sentiment_data.append((sentiment_label, sentiment_probabilities))
+
+    # Add the sentiment data to the news dataframe
+    news["Sentiment"] = sentiment_data
+
+
+    # Return the news dataframe with sentiment analysis results
+    return news
+
+@st.cache_data()
+def bullishness(news_analyzed):
+    positive = 0
+    negative = 0
+    neutral = 0 
+    for i in range(0, len(news_analyzed)):
+        if news_analyzed["Sentiment"].iloc[i][0] == "positive":
+            positive += 1
+        elif news_analyzed["Sentiment"].iloc[i][0] == "negative":
+            negative += 1
+        else:
+            neutral += 1
+
+    bullishness = round((positive) / (positive + negative + neutral) *100, 2)
+    return bullishness
+
+############################################################################################################
+# Streamlit App
+###########################################################################################################
+st.set_page_config(page_title="S&P 500 Stocks Bullishness Sentiment", page_icon="📈", layout="wide")
+
+
+st.sidebar.title("Navigation")
+
+page = st.sidebar.selectbox("Select a page", ["Stock Analysis", "Read News"])
+if page == "Stock Analysis":
+    col1, col2 = st.columns([1,2])
+
+    with col1:
+        st.image("bull-illustration-vector.jpg", use_column_width=True)
+    with col2:
+        st.title('S&P 500 Stocks Bullishness Sentiment')
+
+    st.write("This app is a Stock Bullishness Sentiment Analysis tool that can be used to analyze and predict S&P 500 stocks bullishness sentiment.The information provided in this app is for informational purposes only and should not be considered as financial advice.")
+
+    options = sp500["Security"].tolist()
+    company = st.selectbox("Select the company to be inspected", options)
+    ticker = sp500[sp500["Security"] == company]["Symbol"].values[0]
+    st.write(f"The current price of {company} ({ticker}) is {get_stock_current_price(ticker)} USD")
+    returns = get_stock_returns(ticker)
+    color = "green" if returns >= 0 else "red"
+    st.markdown(f"The returns of {ticker} in the last 30 days is <span style='color: {color}; font-weight: bold;'>{returns}%</span>", unsafe_allow_html=True)
+
+    data = get_stock_series(ticker)
+
+    fig = go.Figure(data=go.Scatter(x=data.index, y=data.values))
+    fig.update_layout(autosize=False, height=300, xaxis_title = "Date", yaxis_title = "USD")  # Change this to your desired height
+    st.plotly_chart(fig, use_container_width=True)
+    
+    news = get_stock_news(company)
+    news_analyzed = analyze_news_sentiment(news)
+    bullishness_sentiment = bullishness(news_analyzed)
+
+    fig = go.Figure(go.Indicator(
+        mode="gauge+number",
+        value=bullishness_sentiment,
+        domain={'x': [0, 1], 'y': [0, 1]},
+        title={'text': "Bullishness Sentiment"},
+        gauge={'axis': {'range': [None, 100]}},
+        ))
+    st.plotly_chart(fig, use_container_width=True)
+
+elif page == "Read News":
+    col1, col2 = st.columns([1,2])
+    with col1:
+        st.image("bull-illustration-vector.jpg", use_column_width=True)
+    with col2:
+        st.title('S&P 500 Stocks Bullishness Sentiment')
