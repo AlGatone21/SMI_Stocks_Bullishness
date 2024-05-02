fc089592e8c2efdeabddd629552456ef8f276ae3
AlGatone21 <163826091+AlGatone21@users.noreply.github.com>
2024-05-02 13:59:14 +0200

    update main

--- smi_stocks_bullishness.py
+++ smi_stocks_bullishness.py
@@ -254,6 +254,8 @@ def app():
             st.write("## Recent News")
 
             for index, row in news.iterrows():
+                st.write(f"### {row['Title']}")
+                st.write(f"Published on {row['Date']} by {row['Publisher']}")
                 st.markdown(row['Description'], unsafe_allow_html=True)
                 st.write("---")
 
