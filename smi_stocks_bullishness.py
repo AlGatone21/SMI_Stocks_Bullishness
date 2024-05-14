e12971c4f32acb351e2bfea7b9712c97906e3226
AlGatone21 <163826091+AlGatone21@users.noreply.github.com>
2024-05-14 21:08:21 +0200

    update

--- smi_stocks_bullishness.py
+++ smi_stocks_bullishness.py
@@ -78,7 +78,7 @@ def get_stock_returns(ticker):
     try:
         data = get_stock_series(ticker)
         returns = (data[-1] - data[0]) / data[0]
-        return returns
+        return round(returns * 100, 2)
     except:
         return "data not available"
 
